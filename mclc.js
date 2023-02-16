da418460862ec5eed7f9ec40e5fb57d29a449eda
Alien <Alien@valhalla.net>
2023-02-16 09:27:50 +0300

    selecting versions

--- mclc.js
+++ mclc.js
@@ -4,18 +4,21 @@ const runMine = () => {
   const launcher = new Client();
 
   let opts = {
+    window:{
+      fullscreen:true
+    },
     clientPackage: null,
     authorization: Authenticator.getAuth("XFD"),
     root: "./minecraft",
     version: {
-      number: "1.14.4",
+      number: "1.7.10",
       type: "release",
     },
     memory: {
       max: "6G",
       min: "4G",
     },
-    forge: "C:/Users/Blockchain/Downloads/forge-1.14.4-28.2.26-installer.jar",
+    // forge: "C:/Users/Blockchain/Downloads/forge-1.14.4-28.2.26-installer.jar",
   };
 
   launcher.launch(opts);
--- preload.js
+++ preload.js
@@ -5,3 +5,20 @@ contextBridge.exposeInMainWorld("versions", {
     ipcRenderer.invoke("launch");
   },
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetch(
+    "https://raw.githubusercontent.com/hugmanrique/mc-versions/main/versions.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const select = document.querySelector(".select-version");
+      data.editions.java.versions.forEach((element) => {
+        console.log(1);
+        const option = document.createElement("option");
+        option.value = element.name;
+        option.textContent = element.name;
+        select.append(option);
+      });
+    });
+});
