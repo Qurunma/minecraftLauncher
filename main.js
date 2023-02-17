0402c807813f43bcc36e8178e248fbd6ea0f709f
Qurunma <yaestgrut75@gmail.com>
2023-02-17 11:24:15 +0300

    setting hidden folder for game

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const launcher = require("./mclc");
 app.disableHardwareAcceleration();
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-  mainWindow.openDevTools();
-  ipcMain.handle("launch", (event, version) => {
-    launcher.launcher(version);
-  });
-  mainWindow.loadFile("index.html");
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
+	mainWindow.openDevTools();
+	ipcMain.handle("launch", (event, version) => {
+		launcher.launcher(version);
+	});
+	mainWindow.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
-  createWindow();
+	createWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
--- mclc.js
+++ mclc.js
@@ -1,32 +1,49 @@
 const runMine = (version) => {
-  const MCLC = require("minecraft-launcher-core");
-  const { Client, Authenticator } = MCLC;
-  const launcher = new Client();
+	const MCLC = require("minecraft-launcher-core");
+	const { Client, Authenticator } = MCLC;
+	const launcher = new Client();
 
-  const os = require("os");
+	const os = require("os");
 
-  let opts = {
-    window: {
-      fullscreen: true,
-    },
-    clientPackage: null,
-    authorization: Authenticator.getAuth("XFD"),
-    root: os.homedir() + "/AppData/Roaming/minecraft",
-    version: {
-      number: version,
-      type: "release",
-    },
-    memory: {
-      max: "6G",
-      min: "4G",
-    },
-    // forge: "C:/Users/Blockchain/Downloads/forge-1.14.4-28.2.26-installer.jar",
-  };
+	// const path = os.homedir() + "/AppData/Roaming/.bao";
 
-  launcher.launch(opts);
+	const fswin = require("fswin");
+	const fs = require("fs");
 
-  launcher.on("debug", (e) => console.log(e));
-  launcher.on("data", (e) => console.log(e));
+	fs.mkdir(os.homedir() + "/AppData/Roaming/.bao", (err) => {});
+
+	// fs.access(os.homedir() + "/AppData/Roaming/.bao", (e) => {
+	// 	console.log(1);
+	// 	if (e) {
+	// 	}
+	// });
+
+	fswin.setAttributesSync(os.homedir() + "/AppData/Roaming/.bao", {
+		IS_HIDDEN: true,
+	});
+
+	let opts = {
+		window: {
+			fullscreen: true,
+		},
+		clientPackage: null,
+		authorization: Authenticator.getAuth("XFD"),
+		root: os.homedir() + "/AppData/Roaming/.bao",
+		version: {
+			number: version,
+			type: "release",
+		},
+		memory: {
+			max: "6G",
+			min: "4G",
+		},
+		// forge: "C:/Users/Blockchain/Downloads/forge-1.14.4-28.2.26-installer.jar",
+	};
+
+	launcher.launch(opts);
+
+	launcher.on("debug", (e) => console.log(e));
+	launcher.on("data", (e) => console.log(e));
 };
 // runMine();
 module.exports.launcher = runMine;
